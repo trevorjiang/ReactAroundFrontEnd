b8671236fe7c5eb62d10a342e59a2b8682cba9c1
Peixuan Jiang <peix8580@esri.com>
2018-01-10 20:47:16 -0800

    Create module header

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Header} from './Header.js'
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
