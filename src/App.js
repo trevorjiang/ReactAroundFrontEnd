50fb749dc49fc99b700d5d4e4581f4dfe0007541
Peixuan Jiang <peix8580@esri.com>
2018-01-15 19:04:49 -0800

    Finish register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Header} from './Header.js'
+import {Main} from './Main.js'
 import './App.css';
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header/>
+        <Main/>
       </div>
     );
   }
