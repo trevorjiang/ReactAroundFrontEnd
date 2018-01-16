35fbcc6a4b8e01e457b4db995188d98f53407410
Peixuan Jiang <peix8580@esri.com>
2018-01-15 21:14:29 -0800

    Add ajax for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import $ from 'jquery';
 import { Link } from 'react-router-dom';
-
-import { Form, Icon, Input, Button} from 'antd';
+import { Form, Icon, Input, Button, message} from 'antd';
+import {API_ROOT} from "../constants"
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
@@ -10,6 +11,20 @@ class NormalLoginForm extends React.Component {
     this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
+        $.ajax({
+          url: `${API_ROOT}/login`,
+          method: 'POST',
+          data: JSON.stringify({
+            username: values.username,
+            password: values.password,
+          }),
+        }).then((response) => {
+          message.success(response);
+        }, (error) => {
+          message.error(error.responseText);
+        }).catch((error) => {
+          console.log(error);
+        })
       }
     });
   }
@@ -18,7 +33,7 @@ class NormalLoginForm extends React.Component {
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <FormItem>
-          {getFieldDecorator('userName', {
+          {getFieldDecorator('username', {
             rules: [{ required: true, message: 'Please input your username!' }],
           })(
             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
