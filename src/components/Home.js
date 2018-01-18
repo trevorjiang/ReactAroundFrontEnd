8151606d6465cf5b8d371e1f42248ae239ca5c5b
Peixuan Jiang <peix8580@esri.com>
2018-01-17 20:16:39 -0800

    Add geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
+import {GEO_OPTIONS} from '../constants';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+  state = {
+    loadingGeoLocation: false,
+  }
+  componentDidMount() {
+    this.setState({loadingGeoLocation: true});
+    this.getGeoLocation();
+  }
+
+  getGeoLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        this.onSuccessLoadGeoLocation,
+        this.onFailLoadGeoLocation,
+        GEO_OPTIONS,
+      );
+    }
+    else {
+
+    }
+  }
+
+  onSuccessLoadGeoLocation = (position) => {
+    console.log(position);
+    this.setState({loadingGeoLocation: false});
+  }
+
+  onFailLoadGeoLocation = (position) => {
+    this.setState({loadingGeoLocation: false});
+  }
+
+  getGalleryPanelContent = () => {
+    return this.state.loadingGeoLocation ? <span>Loading geo location</span> : null
+  }
   render() {
     return (
         <Tabs tabBarExtraContent={operations} className="main-tabs">
-          <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
+          <TabPane tab="Posts" key="1">
+            {
+              this.getGalleryPanelContent()
+            }
+          </TabPane>
           <TabPane tab="Map" key="2">Content of tab 2</TabPane>
         </Tabs>
     );
