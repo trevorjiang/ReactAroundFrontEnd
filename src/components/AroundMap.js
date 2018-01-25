8bd9af16cb23db0435c6e65eaf5d9af9c3683172
Peixuan Jiang <peix8580@esri.com>
2018-01-24 20:31:26 -0800

    Add marker

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {POS_KEY} from "../constants";
 const {
   withScriptjs,
   withGoogleMap,
@@ -16,19 +17,26 @@ class AroundMap extends React.Component {
     this.setState((prevState) => ({isOpen: !prevState.isOpen}));
   }
   render() {
+    const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
     return (
       <GoogleMap
         defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        defaultCenter={{ lat: lat, lng: lon }}
       >
-        <Marker
-          position={{ lat: -34.397, lng: 150.644 }}
-          onClick={this.onToggleOpen}
-        >
-          {this.state.isOpen ? <InfoWindow onCloseClick={this.onToggleOpen}>
-            <div>Text</div>
-          </InfoWindow>: null}
-        </Marker>
+        {
+          this.props.posts.map((post) =>
+            <Marker
+              position={{ lat: post.location.lat, lng: post.location.lon}}
+              onClick={this.onToggleOpen}
+              key={post.url}
+            >
+              {this.state.isOpen ?
+                <InfoWindow onCloseClick={this.onToggleOpen}>
+                <div>Text</div>
+              </InfoWindow>: null}
+            </Marker>
+          )
+        }
       </GoogleMap>
     )
   }
